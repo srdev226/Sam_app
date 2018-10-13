d8c00d2f1d1613e340cf7ac630f337d0cc091484
= <urazovm@hotmail.com>
2018-10-13 16:20:24 -0700

    Finish your signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,40 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+
+  end
+
+  def index
+
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
